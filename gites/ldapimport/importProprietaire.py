3484b9659bb1fdec454559039540d1dc1afe36b2
Martin Peeters _martin.peeters@affinitic.be_ <martin.peeters@affinitic.be>
2013-07-19 10:35:50 +0200

    Fixes the authentication to postgres and ldap. refs #5147

--- gites/ldapimport/importProprietaire.py
+++ gites/ldapimport/importProprietaire.py
@@ -7,6 +7,9 @@ Copyright by Affinitic sprl
 
 $Id$
 """
+
+from affinitic.pwmanager.pwmanager import PasswordManager
+
 from gites.ldapimport.proprietaire import Proprietaire
 from gites.ldapimport.pg import PGDB
 from gites.ldapimport.ldapConnection import LDAP
@@ -62,8 +65,16 @@ class ImportProprietaire(object):
 
 
 def main():
-    pg = PGDB('jfroche', 'xMLMY4', 'localhost', 5432, 'gites_wallons')
-    ldap = LDAP('ldap://localhost', 'cn=admin,dc=gitesdewallonie,dc=net', 'phoneph0ne')
+    pg_pass = PasswordManager()
+    pg_pass.registerFromFile('pgpass', 'pgpass', ':', None, True)
+    pg_pass = pg_pass.getLoginPass()
+    pg = PGDB(pg_pass[0], pg_pass[1], 'localhost', 5432, 'gites_wallons')
+
+    ldap_pass = PasswordManager()
+    ldap_pass.registerFromFile('ldappass', 'ldappass', ':', None, True)
+    ldap_pass = ldap_pass.getLoginPass()
+    ldap = LDAP('ldap://%s' % ldap_pass[0],
+                'cn=admin,dc=gitesdewallonie,dc=net', ldap_pass[1])
     proprioImport = ImportProprietaire(pg, ldap)
     proprioImport.connect()
     #proprioImport.createLdiff()
